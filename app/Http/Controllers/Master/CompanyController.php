9f1dd8f9dc66122cf0d7525767d342757d0dc115
Tubagus Ahmad Marzuqi <tubagus.marzuqi@ukrida.ac.id>
2020-12-12 22:31:44 +0700

    master company

---
+++ app/Http/Controllers/Master/CompanyController.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Http\Controllers\Master;
+
+use App\Http\Controllers\Controller;
+// use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Http\Models\AppLog;
+use App\Http\Models\User;
+use App\Http\Models\Role;
+use App\Http\Models\Company;
+use App\Http\Models\Active;
+use DB;
+
+class CompanyController extends Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->data['header_data']['js'] = array('._data.company');
+    }
+    
+    public function index(Request $request){
+        return view('_page._data.index-company',$this->data);
+    }
+    
+    public function get(Request $request){
+
+        $columns = array(
+            0 =>'id',
+            1 =>'code',
+            2 =>'name',
+            3 =>'active',
+        );
+        $limit = $request->input('length');
+        $start = $request->input('start');
+        $order = $columns[$request->input('order.0.column')];
+        $dir = $request->input('order.0.dir');
+
+        // DB::enableQueryLog(); // Enable query log
+        $models =  DB::table('ms_company')
+                        ->select('*');
+                        // ->leftJoin('ms_role as r', 'u.role_id', '=', 'r.id')
+                        // ->leftJoin('ms_company as c', 'u.company_id', '=', 'c.id');
+                        // ->where('u.active','=',1);
+        if(!empty($request->input('search.value')))
+        {
+            $search = $request->input('search.value');
+            $models = $models->where(function($query) use ($search){
+                        $query->where('name','LIKE',"%{$search}%");
+                    });
+        }
+        $models = $models->offset($start)
+                    ->limit($limit)
+                    ->orderBy($order,$dir)
+                    ->get();
+        // dd(DB::getQueryLog()); // Show results of log
+
+        $recordsFiltered = count(get_object_vars($models));        
+        $recordsTotal = User::count(); // where('active','=',1)
+
+        $data = array();
+        if(!empty($models)) {
+            
+            foreach ($models as $model) {
+                $nestedData=array();
+                $nestedData[] = null;
+                $nestedData[] = $model->code;
+                $nestedData[] = $model->name;
+                $nestedData[] = ($model->active? '<i class="feather icon-check ft-blue-band"></i>':'');
+                $action= "
+                    <span class='action-edit' data-hash='".md5($model->id)."' data-title=''>
+                        <i class='feather icon-edit'></i>
+                    </span>
+                    <span class='action-delete' data-hash='".md5($model->id)."' data-title=''>
+                        <i class='feather icon-trash'></i>
+                    </span>
+                ";
+                $nestedData[] = $action;
+                $data[] = $nestedData;
+            }
+        }
+
+        $json_data = array(
+            "draw"            => intval($request->input('draw')),
+            "recordsTotal"    => intval($recordsTotal),
+            "recordsFiltered" => intval($recordsFiltered),
+            "data"            => $data
+        );
+
+        return json_encode($json_data);
+    }
+
+    // public function detailAdd(){
+    //     $list_role = Role::where('active','=',1)->get();
+    //     $list_company = Company::where('active','=',1)->get();
+
+    //     $data = array(
+    //         "list_role"=>$list_role,
+    //         "list_company"=>$list_company,
+    //     );
+        
+    //     return response()->json($data);
+    // }
+
+    public function doAdd(Request $request){
+        unset($request['_token']);
+        $item = $request->get('params');
+        $item['created_by'] = \Session::get('_user')['_id'];
+        $msg = 'to add company <b>'.$item['name'].'</b>';
+
+        try{
+            Company::insertGetId($item);
+            $output = array('status'=>true, 'message'=>'Success '.$msg);
+        }catch(\Exception $e){
+            $output = array('status'=>false, 'message'=>'Failed '.$msg, 'detail'=>$e->getData());
+        }
+
+        AppLog::createLog('add company',$item,$output);
+        return json_encode($output);
+    }
+
+    public function detailEdit($id){
+        $item = Company::where(DB::raw('md5(id)'),'=',$id)->first();
+
+        $data = array(
+            "detail"=>$item,
+        );
+        
+        return response()->json($data);
+    }
+
+    public function doEdit(Request $request){
+        unset($request['_token']);
+        $item = $request->get('params');
+        $item['updated_by'] = \Session::get('_user')['_id'];
+        $id = $item['id'];
+        unset($item['id']);
+        $msg = 'to edit Company <b>'.$item['name'].'</b>';
+
+        try{
+            Company::where(DB::raw('md5(id)'),'=',$id)->update($item);
+            $output = array('status'=>true, 'message'=>'Success '.$msg);
+        }catch(\Exception $e){
+            $output = array('status'=>false, 'message'=>'Failed '.$msg, 'detail'=>$e->getData());
+        }
+
+        AppLog::createLog('edit company',$item,$output);
+        return json_encode($output);
+    }
+
+    public function delete($ids){ // the id in hash 
+        $array_id = explode(",",$ids);
+        $msg = 'to delete company';
+        $deletedRows = 0;
+        
+        try{
+            foreach ($array_id as $id) {
+                Company::where(DB::raw('md5(id)'),'=',$id)->delete();
+                $deletedRows++;
+            }
+            
+            if($deletedRows >= 1){
+                $s = ($deletedRows > 1) ? "'s" : "";
+                $output = array('status'=>true, 'message'=>'Success '.$msg.$s.' ['.$deletedRows.' row'.$s.']');
+            }else{
+                $output = array('status'=>false, 'message'=>'Selected data unavailable in database', 'detail'=>'');
+            }
+        }catch(Exception $e){
+            $output = array('status'=>false, 'message'=>'Failed '.$msg, 'detail'=>$e->getData());
+        }
+        
+        AppLog::createLog('delete company',$ids,$output);
+        return json_encode($output);
+    }
+}
---
+++ resources/views/_script/_page/_data/company.blade.php
@@ -0,0 +1,425 @@
+<script type="text/javascript">
+//Dropzone script
+    Dropzone.autoDiscover = false;
+    $(document).ready(function() {
+        "use strict"
+        
+        let page_title = 'Company';
+
+        // init view datatable
+        $(document).on("preInit.dt", function () {
+            var $sb = $(".dataTables_filter input[type='search']");
+            $sb.off(); // remove current handler
+            $sb.on("keypress", function (evtObj) { // Add key hander
+                if (evtObj.keyCode == 13) {
+                    if($sb.val().length >= 3 || $sb.val().length == 0){
+                        $(".data-thumb-view").DataTable().search($sb.val()).draw();
+                    }else{
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Do a search using at least 3 characters or leave blank and press enter to cancel the search.',
+                            type: "error",
+                            buttonsStyling: false,
+                            confirmButtonClass: "btn btn-error"
+                        });
+                    }
+                }
+            });
+        });
+        
+        var dataListView = $(".data-thumb-view").DataTable({
+            processing: true,
+            serverSide: true,
+            responsive: false,
+            ajax: {
+            url: "{{ url('master/data/company/get') }}",
+            type: 'GET',
+                error:function(xhr, status, error){
+
+                }
+            },
+            columnDefs: [
+                {
+                    orderable: true,
+                    targets: 0,
+                    checkboxes: { selectRow: true }
+                },
+                {
+                    targets: 'no-sort',
+                    orderable: false,
+                }
+            ],
+            dom: '<"top"<"actions action-btns"B><"action-filters"lf>><"clear">rt<"bottom"<"actions">p>',
+            oLanguage: {
+                sLengthMenu: "_MENU_",
+                sSearch: "",
+                sSearchPlaceholder: "Type and press enter..."
+            },
+            aLengthMenu: [[10, 50, 100, 200], [10, 50, 100, 200]],
+            select: {
+                style: "multi"
+            },
+            order: [[1, "asc"]],
+            bInfo: false,
+            pageLength: 10,
+            buttons: [
+                {
+                    text: "<i class='feather icon-plus'></i> Add New",
+                    action: function() {
+                    $(this).removeClass("btn-secondary")
+                    $(".add-new-data-create").addClass("show")
+                    $(".overlay-bg-create").addClass("show")
+                    },
+                    className: "btn-outline-primary"
+                }
+            ],
+            initComplete: function(settings, json) {
+            $(".dt-buttons .btn").removeClass("btn-secondary")
+            }
+        })
+
+        dataListView.on('draw.dt', function(){
+            setTimeout(function(){
+            if (navigator.userAgent.indexOf("Mac OS X") != -1) {
+                $(".dt-checkboxes-cell input, .dt-checkboxes").addClass("mac-checkbox")
+            }
+            }, 50);
+        });
+
+        // To append actions dropdown before add new button
+        var actionDropdown = $(".actions-dropodown")
+        actionDropdown.insertBefore($(".top .actions .dt-buttons"))
+
+        // Scrollbar
+        if ($(".data-items").length > 0) {
+            new PerfectScrollbar(".data-items", { wheelPropagation: false });
+        }
+
+        if ($(".data-items-update").length > 0) {
+            new PerfectScrollbar(".data-items-update", {
+                wheelPropagation: false,
+                suppressScrollX: true
+            });
+        }
+
+        // Close sidebar
+        $(".hide-data-sidebar, .cancel-data-btn, .overlay-bg").on("click", function() {
+            $(".add-new-data-create").removeClass("show")
+            $(".add-new-data-update").removeClass("show")
+            $(".overlay-bg").removeClass("show")
+            $("#data-name, #data-price").val("")
+            $("#data-category, #data-status").prop("selectedIndex", 0)
+        })
+        
+        // ------------------------------------------- BEGIN :: CRUD
+        // On Add (UI)
+        $(".dt-buttons").on('click', function(event){    
+            let action = 'add';
+            $("#add_body").html(createForm(action));
+        });
+
+        // On Edit (UI)
+        $('div').on("click","span.action-edit",function(e){
+            e.stopPropagation();
+            var hash = $(this).data("hash");
+            var title = $(this).data("title");
+            $(".add-new-data-update").addClass("show");
+            $(".overlay-bg-update").addClass("show");
+            $("#update_title").html("Update Data : "+title);
+            $(".data-items-update").scrollTop(0);
+            $("#update_body").html($("#loading").html());
+            $.ajax({
+                url: 'company/'+hash+'/detailEdit',
+                type:"GET",
+                success:(function(data){
+                    console.log(data);
+                    let action = 'edit';
+                    $("#update_body").html(createForm(action));
+
+                    $("#"+action+"-id").val(hash); 
+                    $("#"+action+"-code").val(data.detail.code); 
+                    $("#"+action+"-name").val(data.detail.name); 
+                    $("#"+action+"-active").prop('checked', (data.detail.active == 1) ? true : false);
+                 
+                }),error:function(xhr,status,error) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: xhr.responseText,
+                        type: "error",
+                        buttonsStyling: false,
+                        confirmButtonClass: "btn btn-error"
+                    });
+                }
+            });
+        });
+
+        // On Delete (UI and DO)
+        $('div').on("click","span.action-delete", function(e){
+            e.stopPropagation();
+            var that = $(this);
+            var hashs = [$(this).data("hash")];
+            var title = $(this).data("title");
+            Swal.fire({
+                title: 'Are you sure to delete '+title+'?',
+                text: "You won't be able to revert this",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                confirmButtonClass: 'btn btn-primary',
+                cancelButtonClass: 'btn btn-outline-primary ml-1',
+                buttonsStyling: false,
+            }).then(function (result) {
+                if (result.value) {
+                    $.ajax({
+                        type:"DELETE",
+                        url: 'company/'+hashs+'/delete',
+                        headers: {
+                            'x-csrf-token': $('meta[name="csrf-token"]').attr('content'),
+                        },
+                        dataType: 'json',
+                        success: function(result){    
+                            if(result['status']){
+                                that.closest('td').parent('tr').fadeOut();
+                                Swal.fire(
+                                    {
+                                        type: "success",
+                                        title: "success",
+                                        text: result['message']+'.',
+                                        confirmButtonClass: 'btn btn-primary',
+                                    }
+                                )
+                                dataListView.ajax.reload();
+                            }else{
+                                Swal.fire({
+                                    title: "Opps.. Error!",
+                                    html: result['message']+'<br><br>'+result['detail'],
+                                    type: "error",
+                                    confirmButtonClass: 'btn btn-primary',
+                                    buttonsStyling: false,
+                                });
+                            }
+                        },
+                        error: function (err){
+                            toastr.error(err, 'Delete Failed!', { "progressBar": true })
+                        }
+                    });
+                }
+            })
+        });
+
+        //on Bulk Delete (UI and DO)
+        $(document).on("click","#bulk_delete", function(){
+            var checked_rows = $('input.dt-checkboxes:checkbox:checked').parents("tr");
+            var hashs = [];
+            if(checked_rows.length) {
+                $.each(checked_rows, function (key, val) {
+                    hashs.push($(this).find(".action-delete").data("hash"))
+                });
+                console.log(hashs);
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "Are you sure you want to delete all selected items? You won't be able to revert this!",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: 'Yes, delete it!',
+                    confirmButtonClass: 'btn btn-primary',
+                    cancelButtonClass: 'btn btn-outline-primary ml-1',
+                    buttonsStyling: false,
+                }).then(function (result) {
+                    if (result.value) {
+                        $.ajax({
+                            type:"DELETE",
+                            url: 'company/'+hashs+'/delete',
+                            headers: {
+                                'x-csrf-token': $('meta[name="csrf-token"]').attr('content'),
+                            },
+                            dataType: 'json',
+                            success: function(result){    
+                                if(result['status']){
+                                    Swal.fire(
+                                        {
+                                            type: "success",
+                                            title: "success",
+                                            text: result['message']+'.',
+                                            confirmButtonClass: 'btn btn-primary',
+                                        }
+                                    )
+                                    dataListView.ajax.reload();
+                                }else{
+                                    Swal.fire({
+                                        title: "Opps.. Error!",
+                                        html: result['message']+'<br><br>'+result['detail'],
+                                        type: "error",
+                                        confirmButtonClass: 'btn btn-primary',
+                                        buttonsStyling: false,
+                                    });
+                                }
+                            },
+                            error: function (err){
+                                toastr.error(err, 'Delete Failed!', { "progressBar": true })
+                            }
+                        });
+                    }
+                });
+            }else{
+                Swal.fire(
+                    {
+                        type: "error",
+                        title: 'Opps..!',
+                        text: 'No items selected, select items to be bulk delete!',
+                        confirmButtonClass: 'btn btn-primary',
+                    }
+                )
+            }
+        });
+
+        // On Blank
+        $('div').on("click","a.action_blank", function(e){
+            e.stopPropagation();
+        });
+
+        // On Add (DO)
+        $("#add_data").on("click",function (){
+            let action = 'add';
+            let action_for_msg = 'Add';
+            let params = getFormData(action);
+
+            $.ajax({
+                type: "POST",
+                url: "company/doAdd",
+                data: {
+                    "_token": "{{ csrf_token() }}",
+                    params: params,
+                },
+                dataType: 'json',
+                success: function(result){        
+                    if(result['status']){
+                        Swal.fire(
+                            {
+                                type: "success",
+                                title: "success",
+                                text: page_title+' has been '+action+'ed.',
+                                confirmButtonClass: 'btn btn-primary',
+                            }
+                        )
+                        dataListView.ajax.reload();
+                        $(".add-new-data-create").removeClass("show")
+                        $(".overlay-bg").removeClass("show")
+                    }else{
+                        toastr.error(result['message']+'<br><br>'+result['detail'], action_for_msg+' Failed!', { "progressBar": true });
+                    }
+                },
+                error: function (err){
+                    toastr.error(err, action_for_msg+' Failed!', { "progressBar": true })
+                }
+            });
+        });
+
+        // On Update (DO)
+        $(document).on("click","#update_data", function(){
+            let action = 'edit';
+            let action_for_msg = 'Edit';
+            let params = getFormData(action);
+
+            $.ajax({
+                type: "POST",
+                url: "company/doEdit",
+                data: {
+                    "_token": "{{ csrf_token() }}",
+                    params: params,
+                },
+                dataType: 'json',
+                success: function(result){        
+                    if(result['status']){
+                        Swal.fire(
+                            {
+                                type: "success",
+                                title: "success",
+                                text: page_title+' has been '+action+'ed.',
+                                confirmButtonClass: 'btn btn-primary',
+                            }
+                        )
+                        dataListView.ajax.reload();
+                        $(".add-new-data-update").removeClass("show")
+                        $(".overlay-bg").removeClass("show")
+                    }else{
+                        toastr.error(result['message']+'<br><br>'+result['detail'], action_for_msg+' Failed!', { "progressBar": true });
+                    }
+                },
+                error: function (err){
+                    toastr.error(err, action_for_msg+' Failed!', { "progressBar": true })
+                }
+            });
+        });
+        // ------------------------------------------- END :: CRUD
+
+        // mac chrome checkbox fix
+        if (navigator.userAgent.indexOf("Mac OS X") != -1) {
+            $(".dt-checkboxes-cell input, .dt-checkboxes").addClass("mac-checkbox")
+        }
+
+        function isUrl(s) {
+            var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+            return regexp.test(s);
+        }
+
+        function getUpdateValue(){
+
+            var name = $(".menu_name").val();
+            var url = $(".update_data_url").val();
+            var sequence = [];
+
+            $(".update_sequence_sibling").each(function(i){
+                sequence[i] = $(this).data("menu-id");
+            });
+
+            return {
+                name: name,
+                url: url,
+                sequence: sequence
+            };
+        }
+
+        function createForm(action){
+            let str =   '<!-- '+action+' form : BEGIN -->\
+                        <div class="col-sm-12 data-field-col">\
+                            <div class="form-group" style="display:none">\
+                                <label for="'+action+'-id"><b>Id</b></label>\
+                                <input type="text" class="form-control" id="'+action+'-id">\
+                            </div>\
+                            <div class="form-group">\
+                                <label for="'+action+'-username"><b>Company Code</b></label>\
+                                <input type="text" class="form-control no-space" id="'+action+'-code" style="text-transform: lowercase" required>\
+                            </div>\
+                            <div class="form-group">\
+                                <label for="'+action+'-fullname"><b>Company Name</b></label>\
+                                <input type="text" class="form-control" id="'+action+'-name" required>\
+                            </div>\
+                            <div class="form-check col-xs-10 col-sm-4" title="check >> for requiring user login and role mapping to access this menu">\
+                                <label for="'+action+'-active"><b>Active</b></label><br>\
+                                <input type="checkbox" class="form-check-input" id="'+action+'-active" value=1>\
+                                yes\
+                            </div>\
+                        </div>\
+                        <!-- '+action+' form : END   -->';
+            return str;
+        }
+
+        function getFormData(action){
+            let data = {
+                code    : $('#'+action+'-code').val(),
+                name    : $('#'+action+'-name').val(),
+                active   : $('#'+action+'-active').is(":checked") ? 1 : 0,
+            };
+
+            if(action == 'edit'){ 
+                data['id'] = $('#'+action+'-id').val();
+            }
+            
+            return data;
+        }
+
+    })
+</script>
