81aef8776a115e697f8d3b67ae6be16ee46a27cc
evelline <evelline.kristiani@kawanlamaretail.com>
2021-02-15 09:54:38 +0700

    bugfix golive

--- app/Http/Controllers/Main/DashboardController.php
+++ app/Http/Controllers/Main/DashboardController.php
@@ -22,60 +22,88 @@ class DashboardController extends Controller
 
     public function index(Request $request){
         // -------------------------------COUNT Member-----------------------------------------------------------
-        $this->data['member']['sum']    = Member::where('active','=',1)
-                                        ->where('member_role_id','=',1)->count();
-        $member_a_year_init             = array_fill(0,12,0);
-        $member_a_year                  = Member::selectRaw("
-                                                EXTRACT(MONTH FROM created_at)-1 AS month,
-                                                count(id) AS value")
-                                        ->where('active','=',1)
-                                        ->where('member_role_id','=',1)
-                                        ->whereYear("created_at",date("Y"))
-                                        ->groupBy('month')
-                                        ->get()
-                                        ->pluck("value","month");
-        $this->data['member']['a_year'] = json_encode(array_replace($member_a_year_init,$member_a_year->toArray()));
+        try{
+            $this->data['member']['sum']    = Member::where('active','=',1)
+                                                    ->where('member_role_id','=',1)->count();
+            $member_a_year_init             = array_fill(0,12,0);
+            $member_a_year                  = Member::selectRaw("
+                                                            EXTRACT(MONTH FROM created_at)-1 AS month,
+                                                            count(id) AS value")
+                                                    ->where('active','=',1)
+                                                    ->where('member_role_id','=',1)
+                                                    ->whereYear("created_at",date("Y"))
+                                                    ->groupBy('month')
+                                                    ->get()
+                                                    ->pluck("value","month");
+            $this->data['member']['a_year'] = json_encode(array_replace($member_a_year_init,$member_a_year->toArray()));
+        }catch(Exception $e) {
+            $this->data['member']['sum']    = null;
+            $this->data['member']['a_year'] = null;
+        }
         // -------------------------------COUNT Personal Trainer--------------------------------------------------
-        $this->data['pt']['sum']        = Member::where('active','=',1)
-                                        ->where('member_role_id','=',3)->count();
-        $pt_a_year_init                 = array_fill(0,12,0);
-        $pt_a_year                      = Member::selectRaw("
-                                                EXTRACT(MONTH FROM created_at)-1 AS month,
-                                                count(id) AS value")
-                                        ->where('active','=',1)
-                                        ->where('member_role_id','=',3)
-                                        ->whereYear("created_at",date("Y"))
-                                        ->groupBy('month')
-                                        ->get()
-                                        ->pluck("value","month");
-        $this->data['pt']['a_year']     = json_encode(array_replace($pt_a_year_init,$pt_a_year->toArray()));
+        try{
+            $this->data['pt']['sum']        = Member::where('active','=',1)
+                                                    ->where('member_role_id','=',3)->count();
+            $pt_a_year_init                 = array_fill(0,12,0);
+            $pt_a_year                      = Member::selectRaw("
+                                                            EXTRACT(MONTH FROM created_at)-1 AS month,
+                                                            count(id) AS value")
+                                                    ->where('active','=',1)
+                                                    ->where('member_role_id','=',3)
+                                                    ->whereYear("created_at",date("Y"))
+                                                    ->groupBy('month')
+                                                    ->get()
+                                                    ->pluck("value","month");
+            $this->data['pt']['a_year']     = json_encode(array_replace($pt_a_year_init,$pt_a_year->toArray()));
+        }catch(Exception $e) {
+            $this->data['pt']['sum']        = null;
+            $this->data['pt']['a_year']     = null;
+        }
         // -------------------------------COUNT Staff-------------------------------------------------------------
-        $this->data['staff']['sum']    = Member::where('active','=',1)
-                                        ->where('member_role_id','=',2)->count();
-        $staff_a_year_init             = array_fill(0,12,0);
-        $staff_a_year                  = Member::selectRaw("
-                                                EXTRACT(MONTH FROM created_at)-1 AS month,
-                                                count(id) AS value")
-                                        ->where('active','=',1)
-                                        ->where('member_role_id','=',2)
-                                        ->whereYear("created_at",date("Y"))
-                                        ->groupBy('month')
-                                        ->get()
-                                        ->pluck("value","month");
-        $this->data['staff']['a_year'] = json_encode(array_replace($staff_a_year_init,$staff_a_year->toArray()));
+        try{
+            $this->data['staff']['sum']    = Member::where('active','=',1)
+                                                    ->where('member_role_id','=',2)->count();
+            $staff_a_year_init             = array_fill(0,12,0);
+            $staff_a_year                  = Member::selectRaw("
+                                                            EXTRACT(MONTH FROM created_at)-1 AS month,
+                                                            count(id) AS value")
+                                                    ->where('active','=',1)
+                                                    ->where('member_role_id','=',2)
+                                                    ->whereYear("created_at",date("Y"))
+                                                    ->groupBy('month')
+                                                    ->get()
+                                                    ->pluck("value","month");
+            $this->data['staff']['a_year'] = json_encode(array_replace($staff_a_year_init,$staff_a_year->toArray()));
+        }catch(Exception $e) {
+            $this->data['staff']['sum']    = null;
+            $this->data['staff']['a_year'] = null;
+        }
         // -------------------------------ABSENCE-----------------------------------------------------------------
-        $this->data['card']['sum']      = $this->data['member']['sum']+$this->data['pt']['sum']+$this->data['staff']['sum'];
-        $this->data['card']['absence']  = MemberActivity::whereDate('created_at', DB::raw('CURDATE()'))
-                                        ->where('transaction_code','like','attend'.'%')
-                                        ->count(DB::raw('DISTINCT card_id'));
+        try{
+            $this->data['card']['sum']      = $this->data['member']['sum']+$this->data['pt']['sum']+$this->data['staff']['sum'];
+            $this->data['card']['absence']  = MemberActivity::whereDate('created_at', DB::raw('CURDATE()'))
+                                            ->where('transaction_code','like','attend'.'%')
+                                            ->count(DB::raw('DISTINCT card_id'));
+        }catch(Exception $e) {
+            $this->data['card']['sum']      = null;
+            $this->data['card']['absence']  = null;
+        }
         // -------------------------------SUBSCRIPTION------------------------------------------------------------
-        $sub                            = Member::selectRaw("
-                                            count(id) as sub")
-                                        ->where('active','=',1)
-                                        ->where('member_role_id','=',1)
-                                        ->groupBy('status_code')
-                                        ->get()->toArray();
-        $this->data['sub']['sum']       = json_encode(array(intval($sub[0]['sub']),intval($sub[1]['sub'])));
+        try{
+            $sub                            = Member::selectRaw("
+                                                        count(id) as sub")
+                                                    ->where('active','=',1)
+                                                    ->where('member_role_id','=',1)
+                                                    ->groupBy('status_code')
+                                                    ->get()->toArray();
+            if($sub){
+                $this->data['sub']['sum']       = json_encode(array(intval($sub[0]['sub']),intval($sub[1]['sub'])));
+            }else{
+                $this->data['sub']['sum']       = null;
+            }
+        }catch(Exception $e) {
+            $this->data['sub']['sum']       = null;
+        }
         // -------------------------------------------------------------------------------------------------------||--DONE
 
         // dd($this->data);
